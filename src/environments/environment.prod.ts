a659a89656b1a38d7816c7613801abd42e5a723b
femithz <ajayiadeyinka6991@gmail.com>
2021-12-05 00:12:20 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   Alphavantage_Api_Key: 'RRUR8007Z0ZJU6JG',
+  Alphavantage_Url: 'https://www.alphavantage.co/',
   Market_Stack_Api_Key: '23092563deadebf9413ee7feb84c4103',
-  MarketStack_Url: 'https://api.marketstack.com/v1/'
+  MarketStack_Url: 'http://api.marketstack.com/v1'
 };
